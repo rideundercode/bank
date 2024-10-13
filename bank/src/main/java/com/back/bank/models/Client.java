c379dcc55feb1dbe0e67b1c9f142a32adae2a032
rideundercode <mohamed.mahtali@gmail.com>
2024-10-13 22:03:43 +0200

    models and application.proporties

---
+++ bank/src/main/java/com/back/bank/models/Client.java
@@ -0,0 +1,50 @@
+package com.back.bank.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // Identifiant unique du client
+
+    private String nom; // Nom du client
+
+    private String email; // Email du client
+
+    // Constructeurs, getters et setters
+    public Client() {
+    }
+
+    public Client(String nom, String email) {
+        this.nom = nom;
+        this.email = email;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
